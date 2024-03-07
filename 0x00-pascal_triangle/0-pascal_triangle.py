8886e5191196ffa654104f135153b1b44ea8749f
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-07 16:36:29 +0100

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
@@ -20,8 +20,9 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
+
 
 # Test the function
 n = 5
