f9ea4098f4002137e0ee59a24b6b317e108406c1
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-22 16:19:43 +0000

    py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,9 +15,11 @@ def minOperations(n):
     """
     if n == 1:
         return 0
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
 
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return i + minOperations(n // i)
-
-    return n
+    return dp[n] if dp[n] != float('inf') else 0
